5e309913588231082db40e772b9152333272752f
vale-rg02 <valeria.rg.24@gmail.com>
2026-06-08 16:54:14 -0700

    Conexión a base de datos en PostgreSQL

--- backend/DespachoJuridico.API/Program.cs
+++ backend/DespachoJuridico.API/Program.cs
@@ -1,8 +1,13 @@
+using DespachoJuridico.API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
