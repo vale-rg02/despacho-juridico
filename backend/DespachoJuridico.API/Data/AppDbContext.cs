f5529dd71feaf504a80d1efc9c4d87c9429ff49c
vale-rg02 <valeria.rg.24@gmail.com>
2026-06-26 22:27:51 -0700

    fix: pruebas para eliminar VistasExpediente

--- backend/DespachoJuridico.API/Data/AppDbContext.cs
+++ backend/DespachoJuridico.API/Data/AppDbContext.cs
@@ -6,7 +6,6 @@ namespace DespachoJuridico.API.Data;
 public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-    public DbSet<VisitaExpediente> VisitasExpediente { get; set; }
 
     public DbSet<Usuario> Usuarios => Set<Usuario>();
     public DbSet<Banco> Bancos => Set<Banco>();
