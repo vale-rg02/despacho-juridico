da5ab86ccd5e3dce56fb5584bb2a8a6c6ff1eb79
vale-rg02 <valeria.rg.24@gmail.com>
2026-06-28 19:21:29 -0700

    fix: restaurar archivos frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,29 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
 import Expedientes from './pages/Expedientes'
 import DetalleExpediente from './pages/DetalleExpediente'
+import NuevoExpediente from './pages/NuevoExpediente'
+import EditarExpediente from './pages/EditarExpediente'
+import RutaProtegida from './components/RutaProtegida'
+import Perfil from './pages/Perfil'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/expedientes" element={<Expedientes />} />
-      <Route path="/expedientes/:id" element={<DetalleExpediente />} />
+      <Route path="/expedientes" element={
+        <RutaProtegida><Expedientes /></RutaProtegida>
+      } />
+      <Route path="/expedientes/nuevo" element={
+        <RutaProtegida><NuevoExpediente /></RutaProtegida>
+      } />
+      <Route path="/expedientes/:id/editar" element={
+        <RutaProtegida><EditarExpediente /></RutaProtegida>
+      } />
+      <Route path="/expedientes/:id" element={
+        <RutaProtegida><DetalleExpediente /></RutaProtegida>
+      } />
+      <Route path="/perfil" element={<Perfil />} />
     </Routes>
   )
 }
